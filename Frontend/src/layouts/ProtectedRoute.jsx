771b002840f270531f33538190955d6954648212
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-10-26 23:24:30 +0500

    Authentication Added in Protected Route for Admin or any other

--- Frontend/src/layouts/ProtectedRoute.jsx
+++ Frontend/src/layouts/ProtectedRoute.jsx
@@ -1,15 +1,38 @@
 import { Navigate } from "react-router-dom";
-import { useAuthStore } from "../app/AuthStore"; // Make sure this path is correct
+import { useAuthStore } from "../app/AuthStore";
+import { useEffect } from "react";
+import { useLoadingStore } from "../app/LoadingStore";
+import Loading from "../components/Admin/Loading";
 
 const ProtectedRoute = ({ element: Component, role, ...rest }) => {
-  const store = useAuthStore();
-  const { accessToken, userRole, isAuthenticated } = store;
+  const { accessToken, userRole, isAuthenticated, verify } = useAuthStore();
+  const { isLoading, setLoading } = useLoadingStore();
 
-  return isAuthenticated && role.includes(userRole?.toUpperCase()) && accessToken ? (
-    <Component {...rest} />
-  ) : (
-    <Navigate to="/login" replace />
-  );
+  useEffect(() => {
+    (async () => {
+      try {
+        if (accessToken && userRole) {
+          await verify();
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error("ProtectedRoute error:", error);
+        window.location.assign("/login");
+      }
+    })();
+  }, [verify, setLoading]);
+
+  if (isLoading) return <Loading />;
+
+  if (
+    isAuthenticated &&
+    role.includes(userRole?.toUpperCase()) &&
+    accessToken
+  ) {
+    return <Component {...rest} />;
+  }
+
+  return <Navigate to="/login" replace />;
 };
 
 export { ProtectedRoute };
