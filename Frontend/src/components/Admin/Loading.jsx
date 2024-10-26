53ac38d93b66f097eb851e403e4039860113eceb
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-10-26 23:23:13 +0500

    Loading Component Added

---
+++ Frontend/src/components/Admin/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="h-[95vh] w-[100%] text-xl font-bold flex justify-center items-center">
+      Loading...
+    </div>
+  );
+};
+
+export default Loading;
