dbd1e32d82fcada08ef544b0fd2f6227307e6915
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-10-26 23:31:55 +0500

    "verifyUser" added in admin route to verify or get the admin

--- Backend/src/routes/admin.route.js
+++ Backend/src/routes/admin.route.js
@@ -8,12 +8,15 @@ import {
     loginAdmin,
     getAdmin,
     updateAdmin,
+    verifyUser,
 } from "../controllers/admin.controller.js";
 
 const router = express.Router();
 
 router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
+router.get("/", verifyJWTUser, verifyUserRole(["ADMIN"]), verifyUser);
+
 router
     .route("/:adminId")
     .get(verifyJWTUser, verifyUserRole(["ADMIN"]), getAdmin)
