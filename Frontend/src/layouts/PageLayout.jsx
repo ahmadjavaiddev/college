6308e375c7ccd35d84f8aba53061edd354cfd888
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-10-30 22:35:02 +0500

    Sidebar Added in PageLayout

--- Frontend/src/layouts/PageLayout.jsx
+++ Frontend/src/layouts/PageLayout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Admin/Header";
+import Sidebar from "../components/Admin/Sidebar";
 
 const PageLayout = () => {
-    return (
-        <>
-            <Header />
-            <main>
-                <Outlet />
-            </main>
-        </>
-    );
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar className="flex-shrink-0" />
+      <main className="flex-grow overflow-auto">
+        <Header />
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default PageLayout;
