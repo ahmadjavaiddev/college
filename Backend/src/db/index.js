c3d1a389e1c2e97dc87bd10cb7dc685befdd0012
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-11-08 10:56:47 +0500

    DB URI changed

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        // const conn = await mongoose.connect(process.env.MONGO_URI);
         const conn = await mongoose.connect(
-            "mongodb+srv://ahmadjavaiddev:justNothing@cluster0.uyh4woy.mongodb.net/college-management"
+            "mongodb://localhost:27017/college-management"
         );
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
