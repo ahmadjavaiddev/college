3b6470016cf8cf88f8d5b668915ad246f2eadace
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-11-11 20:32:14 +0500

    ProtectedRoute navigate improved

--- Frontend/src/layouts/ProtectedRoute.jsx
+++ Frontend/src/layouts/ProtectedRoute.jsx
@@ -5,37 +5,40 @@ import { useLoadingStore } from "../app/LoadingStore";
 import Loading from "../components/Admin/Loading";
 
 const ProtectedRoute = ({ element: Component, role, ...rest }) => {
-  const { accessToken, userRole, isAuthenticated, verify, logout } =
-    useAuthStore();
-  const { isLoading, setLoading } = useLoadingStore();
-  const navigate = useNavigate();
+    const { accessToken, userRole, isAuthenticated, verify, logout } =
+        useAuthStore();
+    const { isLoading, setLoading } = useLoadingStore();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    (async () => {
-      try {
-        if (accessToken && userRole) {
-          await verify();
-        }
-        setLoading(false);
-      } catch (error) {
-        console.error("ProtectedRoute error:", error);
-        logout();
-        navigate("/login");
-      }
-    })();
-  }, [verify, setLoading]);
+    useEffect(() => {
+        (async () => {
+            try {
+                if (accessToken && userRole) {
+                    const response = await verify();
+                    if (!response) {
+                        logout();
+                        navigate("/login");
+                    }
+                }
+                setLoading(false);
+            } catch (error) {
+                console.error("ProtectedRoute error:", error);
+                logout();
+            }
+        })();
+    }, [verify, setLoading]);
 
-  if (isLoading) return <Loading />;
+    if (isLoading) return <Loading />;
 
-  if (
-    isAuthenticated &&
-    role.includes(userRole?.toUpperCase()) &&
-    accessToken
-  ) {
-    return <Component {...rest} />;
-  }
+    if (
+        isAuthenticated &&
+        role.includes(userRole?.toUpperCase()) &&
+        accessToken
+    ) {
+        return <Component {...rest} />;
+    }
 
-  return <Navigate to="/login" replace />;
+    return <Navigate to="/login" replace />;
 };
 
 export { ProtectedRoute };
