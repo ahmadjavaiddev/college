e2412f7f72a998f604064054a91d72e7cc804d52
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-11-12 05:19:45 +0500

    "requestHandler" added to hanfle API requests efficiently

--- Frontend/src/utils/index.js
+++ Frontend/src/utils/index.js
@@ -1,61 +1,49 @@
 export class LocalStorage {
-  // Get a value from local storage by key
-  static get(key) {
-    const value = localStorage.getItem(key);
-    if (value) {
-      try {
-        return JSON.parse(value);
-      } catch (err) {
+    // Get a value from local storage by key
+    static get(key) {
+        const value = localStorage.getItem(key);
+        if (value) {
+            try {
+                return JSON.parse(value);
+            } catch (err) {
+                return null;
+            }
+        }
         return null;
-      }
-    }
-    return null;
-  }
-
-  // Set a value in local storage by key
-  static set(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
-  }
-
-  // Remove a value from local storage by key
-  static remove(key) {
-    localStorage.removeItem(key);
-  }
-
-  // Clear all items from local storage
-  static clear() {
-    localStorage.clear();
-  }
-}
-
-export function transformLecturesData(data) {
-  const transformed = [];
-
-  data.forEach((item) => {
-    // Find if a day group already exists for the current section and day
-    let dayGroup = transformed.find(
-      (group) => group.section === item.section && group.day === item.day
-    );
-
-    // If no group found, create a new one and add it to the transformed array
-    if (!dayGroup) {
-      dayGroup = {
-        _id: item._id,
-        section: item.section,
-        day: item.day,
-        lectures: [],
-      };
-      transformed.push(dayGroup);
     }
 
-    // Add the lecture details to the lectures array of the day group
-    dayGroup.lectures.push({
-      teacher: item.teacher,
-      subject: item.subject,
-      time: item.time,
-      _id: item._id,
-    });
-  });
+    // Set a value in local storage by key
+    static set(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
 
-  return transformed;
+    // Remove a value from local storage by key
+    static remove(key) {
+        localStorage.removeItem(key);
+    }
+
+    // Clear all items from local storage
+    static clear() {
+        localStorage.clear();
+    }
 }
+
+export const requestHandler = async (api, onSuccess, onError) => {
+    try {
+        // Make the API request
+        const response = await api();
+        const { data } = response;
+        if (data?.success) {
+            // Call the onSuccess callback with the response data
+            onSuccess(data.data);
+        }
+    } catch (error) {
+        // Handle error cases, including unauthorized and forbidden cases
+        if ([401, 403].includes(error?.response.data?.statusCode)) {
+            localStorage.clear(); // Clear local storage on authentication issues
+            window.location.href = "/login"; // Redirect to login page
+        }
+        onError &&
+            onError(error?.response?.data?.message || "Something went wrong");
+    }
+};
