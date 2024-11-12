f9fb816c741fc9158fd35686e1ae6a08f104476c
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-11-12 05:23:01 +0500

    "AddLecture" functionality added to add lectures

--- Frontend/src/components/Admin/Lectures/AddLecture.jsx
+++ Frontend/src/components/Admin/Lectures/AddLecture.jsx
@@ -1,107 +1,138 @@
 import { useState } from "react";
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  Button,
-  Input,
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+    Button,
+    Input,
+    Select,
+    SelectContent,
+    SelectTrigger,
+    SelectValue,
+    SelectItem,
 } from "@/components/ui/index.js";
+import { useEffect } from "react";
+import useLectureStore from "../../../app/useLectureStore";
 
-const AddLecture = () => {
-  const [newLecture, setNewLecture] = useState({
-    subject: "",
-    startTime: 0,
-    endTime: 0,
-    day: "",
-  });
+const AddLecture = ({ AddLectureDay, setShowAddLectureForm }) => {
+    const [newLecture, setNewLecture] = useState({
+        subject: "",
+        day: AddLectureDay?.toLowerCase() || "",
+        teacher: "",
+    });
+    const { fetchTeacherFormData, teachersFormData, addNewLecture } =
+        useLectureStore();
 
-  const handleAddLecture = () => {};
+    useEffect(() => {
+        fetchTeacherFormData();
+    }, []);
 
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Add New Lecture</CardTitle>
-      </CardHeader>
-      <CardContent className="space-y-4">
-        <div>
-          <label
-            htmlFor="title"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Title
-          </label>
-          <Input
-            id="title"
-            value={newLecture.subject}
-            onChange={(e) =>
-              setNewLecture({ ...newLecture, subject: e.target.value })
-            }
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="startTime"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Start Time
-          </label>
-          <Input
-            id="startTime"
-            type="number"
-            value={newLecture.startTime}
-            onChange={(e) =>
-              setNewLecture({
-                ...newLecture,
-                startTime: parseInt(e.target.value, 10),
-              })
-            }
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="endTime"
-            className="block text-sm font-medium text-gray-700"
-          >
-            End Time
-          </label>
-          <Input
-            id="endTime"
-            type="number"
-            value={newLecture.endTime}
-            onChange={(e) =>
-              setNewLecture({
-                ...newLecture,
-                endTime: parseInt(e.target.value, 10),
-              })
-            }
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="day"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Day
-          </label>
-          <Input
-            id="day"
-            type="text"
-            value={newLecture.day}
-            onChange={(e) =>
-              setNewLecture({
-                ...newLecture,
-                day: e.target.value,
-              })
-            }
-          />
-        </div>
-        <Button onClick={handleAddLecture} className="w-full">
-          Add Lecture
-        </Button>
-      </CardContent>
-    </Card>
-  );
+    const handleAddLecture = async () => {
+        const response = await addNewLecture(newLecture);
+        if (response) {
+            console.log("response in handleAddLecture ::", response);
+            setShowAddLectureForm(false);
+        }
+    };
+
+    const handleDayChange = (value) => {
+        setNewLecture({
+            ...newLecture,
+            day: value,
+        });
+    };
+
+    const handleTeacherChange = (value) => {
+        setNewLecture({
+            ...newLecture,
+            teacher: value,
+        });
+    };
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Add New Lecture</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-4">
+                <div>
+                    <label
+                        htmlFor="title"
+                        className="block text-sm font-medium text-gray-700"
+                    >
+                        Title
+                    </label>
+                    <Input
+                        id="title"
+                        value={newLecture.subject}
+                        onChange={(e) =>
+                            setNewLecture({
+                                ...newLecture,
+                                subject: e.target.value,
+                            })
+                        }
+                    />
+                </div>
+                <div>
+                    <label
+                        htmlFor="teacher"
+                        className="block text-sm font-medium text-gray-700"
+                    >
+                        Teacher
+                    </label>
+                    <Select onValueChange={handleTeacherChange}>
+                        <SelectTrigger>
+                            <SelectValue placeholder="Select Teacher" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {teachersFormData?.map((item) => (
+                                <SelectItem value={item?._id} key={item?._id}>
+                                    {item?.firstName} {item?.lastName}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div>
+                    <label
+                        htmlFor="day"
+                        className="block text-sm font-medium text-gray-700"
+                    >
+                        Day
+                    </label>
+                    <Select
+                        defaultValue={newLecture.day || ""}
+                        onValueChange={handleDayChange}
+                    >
+                        <SelectTrigger>
+                            <SelectValue placeholder="Select Day" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {[
+                                "Monday",
+                                "Tuesday",
+                                "Wednesday",
+                                "Thursday",
+                                "Friday",
+                                "Saturday",
+                            ].map((item) => (
+                                <SelectItem
+                                    value={item?.toLowerCase()}
+                                    key={item}
+                                >
+                                    {item}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                </div>
+                <Button onClick={handleAddLecture} className="w-full">
+                    Add Lecture
+                </Button>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default AddLecture;
