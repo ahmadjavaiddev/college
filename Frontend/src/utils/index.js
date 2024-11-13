5ee8e62c85ce6ef8055167883eca80426b23f5c8
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-11-14 03:58:13 +0500

    Refactor requestHandler function to include toast messages

--- Frontend/src/utils/index.js
+++ Frontend/src/utils/index.js
@@ -1,3 +1,5 @@
+import { toast } from "@/hooks/use-toast";
+
 export class LocalStorage {
     // Get a value from local storage by key
     static get(key) {
@@ -28,14 +30,15 @@ export class LocalStorage {
     }
 }
 
-export const requestHandler = async (api, onSuccess, onError) => {
+export const requestHandler = async (api, onSuccess, message, errorMessage) => {
     try {
         // Make the API request
         const response = await api();
         const { data } = response;
         if (data?.success) {
             // Call the onSuccess callback with the response data
-            onSuccess(data.data);
+            onSuccess(data.data ? data.data : data);
+            message && toast({ title: message });
         }
     } catch (error) {
         // Handle error cases, including unauthorized and forbidden cases
@@ -43,7 +46,12 @@ export const requestHandler = async (api, onSuccess, onError) => {
             localStorage.clear(); // Clear local storage on authentication issues
             window.location.href = "/login"; // Redirect to login page
         }
-        onError &&
-            onError(error?.response?.data?.message || "Something went wrong");
+
+        toast({
+            variant: "destructive",
+            title: `Uh oh! ${
+                errorMessage ? errorMessage : "SomeThing went wrong!"
+            }.`,
+        });
     }
 };
