47d9ae6c8d6bc00ff609064ce70eac7426c71584
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-11-16 00:37:02 +0500

    Refactor PageLayout.jsx to add sticky header with white background

--- Frontend/src/layouts/PageLayout.jsx
+++ Frontend/src/layouts/PageLayout.jsx
@@ -12,7 +12,7 @@ const PageLayout = () => {
         <SidebarProvider>
             <Sidebar />
             <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                <header className="flex bg-white sticky top-0 z-50 h-14 shrink-0 items-center gap-2 border-b px-4">
                     <SidebarTrigger className="-ml-1" />
                 </header>
                 <main className="flex flex-1 flex-col gap-4 p-0">
