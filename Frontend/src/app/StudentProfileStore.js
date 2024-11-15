18a062322321076125cc7c0f1580cd9697dbe316
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-11-15 20:54:29 +0500

    Refactor StudentProfileStore.js to handle fetching student data and manage loading state

--- Frontend/src/app/StudentProfileStore.js
+++ Frontend/src/app/StudentProfileStore.js
@@ -1,10 +1,34 @@
 import { create } from "zustand";
+import { requestHandler } from "../utils";
+import { getStudentData } from "../api";
 
-export const useStudentProfileStore = create((set) => ({
+export const useStudentProfileStore = create((set, get) => ({
+    students: [],
     student: {},
+    loading: true,
 
-    setStudent: (studentData) =>
-        set(() => ({
-            student: studentData,
-        })),
+    fetchStudent: async (studentId) => {
+        const studentInState = get().students.find(
+            (student) => student._id === studentId
+        );
+
+        if (studentInState) {
+            set({ student: studentInState, loading: false });
+            return studentInState;
+        }
+
+        await requestHandler(
+            async () => await getStudentData(studentId),
+            (res) => {
+                const students = [...get().students, res.user];
+                set({ students: students, student: res.user, loading: false });
+            },
+            () => set({ student: null, loading: false }),
+            null,
+            "Student not found"
+        );
+
+        const fetchedStudent = get().student;
+        return fetchedStudent;
+    },
 }));
