de56727b1f4815f111e87f67bf5f1f6d0dc430d0
AhmadJavaidDev <ahmadjavaiddev@gmail.com>
2024-11-15 20:54:49 +0500

    Add StudentError component to handle "Student Not Found" error

---
+++ Frontend/src/components/Admin/Students/Error.jsx
@@ -0,0 +1,7 @@
+export function StudentError() {
+    return (
+        <div className="flex justify-center items-center h-[90vh] text-xl">
+            Uh oh! Student Not Found!
+        </div>
+    );
+}
